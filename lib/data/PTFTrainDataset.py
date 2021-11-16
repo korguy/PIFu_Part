35540ccd99a6d6077519bc6a20799d75f0055cec
KORguy <hyun.kang@hotmail.com>
2021-11-17 00:21:43 +0900

    debug

--- lib/data/PTFTrainDataset.py
+++ lib/data/PTFTrainDataset.py
@@ -44,51 +44,7 @@ def load_trimesh(root_dir):
         #### mesh = trimesh.load("alvin_t_posed.obj",process=False, maintain_order=True, skip_uv=True)
 
     return meshs
-
-def save_samples_truncated_part(fname, points, part):
-    '''
-    points: [N, 3] points sampled from mesh
-    part: [N, 20] one hot vector representation
-    '''
-    part_idx = np.argmax(part, axis=1)
-    r = ((255/20)*part_idx).reshape((-1, 1))
-    g = (255//(part_idx+1)).reshape((-1, 1))
-    b = (255/20*(20-part_idx)).reshape((-1, 1))
     
-    to_save = np.concatenate([points, r,g, b], axis=-1)
-    return np.savetxt(fname,
-                  to_save,
-                  fmt='%.6f %.6f %.6f %d %d %d',
-                  comments='',
-                  header=(
-                      'ply\nformat ascii 1.0\nelement vertex {:d}\nproperty float x\nproperty float y\nproperty float z\nproperty uchar red\nproperty uchar green\nproperty uchar blue\nend_header').format(
-                      points.shape[0])
-                  )
-    
-def save_samples_truncted_prob(fname, points, prob):
-    '''
-    Save the visualization of sampling to a ply file.
-    Red points represent positive predictions.
-    Green points represent negative predictions.
-    :param fname: File name to save
-    :param points: [N, 3] array of points
-    :param prob: [N, 1] array of predictions in the range [0~1]
-    :return:
-    '''
-    r = (prob > 0.5).reshape([-1, 1]) * 255
-    g = (prob < 0.5).reshape([-1, 1]) * 255
-    b = np.zeros(r.shape)
-
-    to_save = np.concatenate([points, r, g, b], axis=-1)
-    return np.savetxt(fname,
-                      to_save,
-                      fmt='%.6f %.6f %.6f %d %d %d',
-                      comments='',
-                      header=(
-                          'ply\nformat ascii 1.0\nelement vertex {:d}\nproperty float x\nproperty float y\nproperty float z\nproperty uchar red\nproperty uchar green\nproperty uchar blue\nend_header').format(
-                          points.shape[0])
-                      )
-
 class PTFTrainDataset(Dataset):
 
     @staticmethod
