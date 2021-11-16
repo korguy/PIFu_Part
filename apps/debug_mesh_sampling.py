445938293b8b3dede1eba6a1835a5f011473a2eb
KORguy <hyun.kang@hotmail.com>
2021-11-16 11:53:32 +0900

    debug

--- apps/debug_mesh_sampling.py
+++ apps/debug_mesh_sampling.py
@@ -3,12 +3,12 @@ import os
 
 
 if __name__ == "__main__":
-	base = "./training_data"
-	subjects = os.listdir(os.path.join(base, "GEO", "OBJ"))
-	for subject in subjects:
-		mesh = trimesh.load(os.path.join(base, "GEO", "OBJ", subject, "%s_posed.obj" % subject), process=False, maintain_order=True, skip_uv=True)
-		surface_points, surface_points_face_indices = trimesh.sample.sample_surface(mesh, 32000)
-		surface_points_faces = mesh.faces[surface_points_face_indices]
+    base = "./training_data"
+    subjects = os.listdir(os.path.join(base, "GEO", "OBJ"))
+    for subject in subjects:
+        mesh = trimesh.load(os.path.join(base, "GEO", "OBJ", subject, "%s_posed.obj" % subject), process=False, maintain_order=True, skip_uv=True)
+        surface_points, surface_points_face_indices = trimesh.sample.sample_surface(mesh, 32000)
+        surface_points_faces = mesh.faces[surface_points_face_indices]
         surface_points_vertices_indices = []
         
         for single_face in surface_points_faces:
@@ -16,13 +16,13 @@ if __name__ == "__main__":
 
         m1 = max(surface_points_vertices_indices)
         with open(os.path.join(base, "PART", subject, "%s_part.json" % subject.split('_')[0])) as f: 
-    		json_data = json.load(f)
-    	m2 = max(map(int, json_data.keys()))
+            json_data = json.load(f)
+        m2 = max(map(int, json_data.keys()))
 
-    	if len(mesh.vertices) != len(json_data.keys()):
-    		print(subject, "has unlabelled vertices")
+        if len(mesh.vertices) != len(json_data.keys()):
+            print(subject, "has unlabelled vertices")
 
-    	if m1 > m2:
-    		print(subject, "sampling error")
+        if m1 > m2:
+            print(subject, "sampling error")
 
 
