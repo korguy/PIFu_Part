1ff3624074b3bab258c533c14f8635bf3a8eb8e5
KORguy <hyun.kang@hotmail.com>
2021-11-16 19:57:53 +0900

    reduced mlp

--- lib/model/HGPIFuPart.py
+++ lib/model/HGPIFuPart.py
@@ -156,7 +156,7 @@ class HGPIFuPart(BasePIFuNet):
         error['Err(occ)'] /= len(self.intermediate_preds_list)
 
         for part in self.intermediate_parts_list:
-            error['Err(part)'] += self.criteria['part'](part, self.gt_parts.long(), reduction='none') * 0.1
+            error['Err(part)'] += F.cross_entropy(part, self.gt_parts.long(), reduction='none') * 0.1
         error['Err(part)'] /= len(self.intermediate_parts_list)
 
         return error
